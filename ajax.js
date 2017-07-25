8c24531747a9cf89e2f88f4e61c42afc7de78c90
sometimes1 <pengyufang520@qq.com>
2017-07-26 01:26:27 +0800

    Update ajax.js

--- ajax.js
+++ ajax.js
@@ -22,10 +22,71 @@ var server = http.createServer(function(request, response){
       response.end(string)   
     }else if(path === '/xxx'){   
      response.setHeader('Content-Type', 'text/html;charset=utf-8')
-      response.end('hello AJAX!')
+      response.end('hello yueyue!')
+    }else if(path === '/main.js'){  
+      var string = fs.readFileSync('./main.js')
+      response.setHeader('Content-Type', 'application/javascript')
+      response.end(string)
     }else{  
       response.statusCode = 404
       response.setHeader('Content-Type', 'text/html;charset=utf-8') 
       response.end('找不到对应的路径，你需要自行修改 index.js')
     }
+  }else if(method === 'POST'){
+    if(path === '/login'){
+      response.setHeader('Content-Type', 'text/html;charset=utf-8') 
+      readBody(request, function(body){
+        let parts = body.split('&')
+        let username = parts[0].split('=')[1]
+        let password = parts[1].split('=')[1]
+        let errors = {} 
+
+        if(username.trim() === ''){
+          errors['username'] = '用户名不能为空'
+        }else if(username === 'frank'){
+          if(password === ''){
+            errors['password'] = '密码不能为空'
+          }else if(password !== '123123'){
+            errors['password'] = '密码错误'
+          }
+        }else{
+          errors['username'] = '用户名不存在'
+        }
+        if(password === ''){
+          errors['password'] = '密码不能为空'
+        }
+
+        if(Object.keys(errors).length > 0){
+          response.statusCode = 412
+          var string = JSON.stringify({errors:errors})
+          response.end(string)
+        }else{
+          response.statusCode = 200
+          response.end(JSON.stringify({userId:1}))
+        }
+      })
+    }else{
+      response.statusCode = 404
+      response.setHeader('Content-Type', 'text/html;charset=utf-8') 
+      response.end('滚')
+    }
   }
+
+  // 代码结束，下面不要看
+  console.log(method + ' ' + request.url)
+})
+
+server.listen(port)
+console.log('监听 ' + port + ' 成功，请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
+
+function readBody(request, callback){
+  let body = [];
+  request.on('error', (err) => {
+    console.error(err);
+  }).on('data', (chunk) => {
+    body.push(chunk);
+  }).on('end', () => {
+    body = Buffer.concat(body).toString();
+    callback.call(null, body)
+  })
+}
